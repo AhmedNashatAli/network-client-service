95c94f7e95c5367e8a646604d10f76985d9d14a3
Ahmed Nashat <anashat@groupon.com>
2020-02-22 22:26:19 +0000

    add tests for muliplication

--- src/test/java/com/ics/network/networkclientservice/NetworkClientServiceApplicationTests.java
+++ src/test/java/com/ics/network/networkclientservice/NetworkClientServiceApplicationTests.java
@@ -1,16 +1,24 @@
 package com.ics.network.networkclientservice;
 
+import com.ics.network.networkclientservice.controller.MultiplicationController;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class NetworkClientServiceApplicationTests {
+    @Autowired
+    private MultiplicationController multiplicationController;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+        assertThat(this.multiplicationController).isNotNull();
+    }
 
 }
